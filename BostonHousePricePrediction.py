02f80fcc0b21dfeb90555729877e4e12530f57d5
MrFlygerian <62527934+MrFlygerian@users.noreply.github.com>
2020-04-08 09:31:39 +0100

    Add files via upload
    Adapted regression code

---
+++ BostonHousePricePrediction.py
@@ -0,0 +1,274 @@
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+# Suppress matplotlib user warnings
+# Necessary for newer version of matplotlib
+import warnings
+warnings.filterwarnings("ignore", category = UserWarning, module = "matplotlib")
+# Display inline matplotlib plots with IPython
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+# Import supplementary visualizations code visuals.py
+#import vpython as vs
+import matplotlib.pyplot as plt
+import seaborn as sns
+#import scikit modules
+from sklearn.metrics import r2_score
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import validation_curve
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import ShuffleSplit, train_test_split
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import NearestNeighbors
+
+
+def ModelLearning(X, y):
+    """ Calculates the performance of several models with varying sizes of training data.
+        The learning and testing scores for each model are then plotted. """
+
+    # Create 10 cross-validation sets for training and testing
+    cv = ShuffleSplit(n_splits = 10, test_size = 0.2, random_state = 42)
+
+    # Generate the training set sizes increasing by 50
+    train_sizes = np.rint(np.linspace(1, X.shape[0]*0.8 - 1, 9)).astype(int)
+
+    # Create the figure window
+    fig = plt.figure(figsize=(10,7))
+
+    # Create three different models based on max_depth
+    for k, depth in enumerate([1,3,6,10]):
+
+        # Create a Decision tree regressor at max_depth = depth
+        regressor = DecisionTreeRegressor(max_depth = depth)
+
+        # Calculate the training and testing scores
+        sizes, train_scores, test_scores = learning_curve(regressor, X, y, \
+            cv = cv, train_sizes = train_sizes, scoring = 'r2')
+
+        # Find the mean and standard deviation for smoothing
+        train_std = np.std(train_scores, axis = 1)
+        train_mean = np.mean(train_scores, axis = 1)
+        test_std = np.std(test_scores, axis = 1)
+        test_mean = np.mean(test_scores, axis = 1)
+
+        # Subplot the learning curve
+        ax = fig.add_subplot(2, 2, k+1)
+        ax.plot(sizes, train_mean, 'o-', color = 'r', label = 'Training Score')
+        ax.plot(sizes, test_mean, 'o-', color = 'g', label = 'Testing Score')
+        ax.fill_between(sizes, train_mean - train_std, \
+            train_mean + train_std, alpha = 0.15, color = 'r')
+        ax.fill_between(sizes, test_mean - test_std, \
+            test_mean + test_std, alpha = 0.15, color = 'g')
+
+        # Labels
+        ax.set_title('max_depth = %s'%(depth))
+        ax.set_xlabel('Number of Training Points')
+        ax.set_ylabel('Score')
+        ax.set_xlim([0, X.shape[0]*0.8])
+        ax.set_ylim([-0.05, 1.05])
+
+    # Visual aesthetics
+    ax.legend(bbox_to_anchor=(1.05, 2.05), loc='lower left', borderaxespad = 0.)
+    fig.suptitle('Decision Tree Regressor Learning Performances', fontsize = 16, y = 1.03)
+    fig.tight_layout()
+    fig.show()
+#----------------------------------------------------------------------------------------------------
+def ModelComplexity(X, y):
+    """ Calculates the performance of the model as model complexity increases.
+        The learning and testing errors rates are then plotted. """
+
+    # Create 10 cross-validation sets for training and testing
+    cv = ShuffleSplit(n_splits = 10, test_size = 0.2, random_state = 42)
+
+    # Vary the max_depth parameter from 1 to 10
+    max_depth = np.arange(1,11)
+
+    # Calculate the training and testing scores
+    train_scores, test_scores = validation_curve(DecisionTreeRegressor(), X, y, \
+        param_name = "max_depth", param_range = max_depth, cv = cv, scoring = 'r2')
+
+    # Find the mean and standard deviation for smoothing
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+    test_mean = np.mean(test_scores, axis=1)
+    test_std = np.std(test_scores, axis=1)
+
+    # Plot the validation curve
+    plt.figure(figsize=(7, 5))
+    plt.title('Decision Tree Regressor Complexity Performance')
+    plt.plot(max_depth, train_mean, 'o-', color = 'r', label = 'Training Score')
+    plt.plot(max_depth, test_mean, 'o-', color = 'g', label = 'Validation Score')
+    plt.fill_between(max_depth, train_mean - train_std, \
+        train_mean + train_std, alpha = 0.15, color = 'r')
+    plt.fill_between(max_depth, test_mean - test_std, \
+        test_mean + test_std, alpha = 0.15, color = 'g')
+
+    # Visual aesthetics
+    plt.legend(loc = 'lower right')
+    plt.xlabel('Maximum Depth')
+    plt.ylabel('Score')
+    plt.ylim([-0.05,1.05])
+    plt.show()
+#------------------------------------------------------------------------------------------------------
+def PredictTrials(X, y, fitter, data):
+    """ Performs trials of fitting and predicting data. """
+
+    # Store the predicted prices
+    prices = []
+
+    for k in range(10):
+        # Split the data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, \
+            test_size = 0.2, random_state = k)
+
+        # Fit the data
+        reg = fitter(X_train, y_train)
+
+        # Make a prediction
+        pred = reg.predict([data[2]])[0]
+        prices.append(pred)
+
+        # Result
+        print(f"\nTrial {k+1}: ${round(pred,0)}")
+    
+
+    # Display price range
+    print(f"\nRange in prices: ${round(max(prices) - min(prices),0)}")
+#---------------------------------------------------------------------------------------------
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the performance score between 
+        true (y_true) and predicted (y_predict) values based on the metric chosen. """
+    
+    score = r2_score(y_true, y_predict)
+    
+    # Return the score
+    return score
+#-----------------------------------------------------------------------------------------------------
+def fit_model(X, y):
+    """ Performs grid search over the 'max_depth' parameter for a 
+        decision tree regressor trained on the input data [X, y]. """
+    
+    # Create cross-validation sets from the training data
+    cv_sets = ShuffleSplit(n_splits = 10, test_size = 0.20, random_state = 42)
+
+    # Create a decision tree regressor object
+    regressor = DecisionTreeRegressor()
+
+    # Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    params = {'max_depth':[1,2,3,4,5,6,7,8,9,10]}
+
+    # Transform 'performance_metric' into a scoring function using 'make_scorer' 
+    scoring_fnc = make_scorer(performance_metric)
+
+    # Create the grid search cv object --> GridSearchCV()
+    grid = GridSearchCV(estimator=regressor, param_grid=params, scoring=scoring_fnc, cv=cv_sets)
+
+    # Fit the grid search object to the data to compute the optimal model
+    grid = grid.fit(X, y)
+
+    # Return the optimal model after fitting the data
+    return grid.best_estimator_
+#----------------------------------------------------------------------------------------------------
+# Create new similar function
+def fit_model_2(X, y):
+    """ Performs grid search over the 'max_depth' parameter for a 
+        decision tree regressor trained on the input data [X, y]. """
+
+    # Create cross-validation sets from the training data
+    # ShuffleSplit works iteratively compared to KFOLD
+    # It saves computation time when your dataset grows
+    # X.shape[0] is the total number of elements
+    # n_iter is the number of re-shuffling & splitting iterations.
+    cv_sets = ShuffleSplit(X.shape[0], test_size = 0.20, random_state = 42)
+
+    # TODO: Create a decision tree regressor object
+    # Instantiate
+    regressor = DecisionTreeRegressor(random_state=0)
+
+    # TODO: Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    dt_range = range(1, 11)
+    params = dict(max_depth=dt_range)
+
+    # TODO: Transform 'performance_metric' into a scoring function using 'make_scorer' 
+    # We initially created performance_metric using R2_score
+    scoring_fnc = make_scorer(performance_metric)
+
+    # TODO: Create the grid search object
+    # You would realize we manually created each, including scoring_func using R^2
+    rand = RandomizedSearchCV(regressor, params, cv=cv_sets, scoring=scoring_fnc)
+
+    # Fit the grid search object to the data to compute the optimal model
+    rand = rand.fit(X, y)
+
+    # Return the optimal model after fitting the data
+    return rand.best_estimator_
+#----------------------------------------------------------------------------------------------------
+def nearest_neighbor_price(x):
+    num_neighbors=5
+    # x is your vector and X is the data set.
+    def find_nearest_neighbor_indexes(x, X):
+        # Instantiate
+        neigh = NearestNeighbors(num_neighbors)
+        # Fit
+        neigh.fit(X)
+        distance, indexes = neigh.kneighbors(x)
+        return indexes
+        # This returns, the position, say for example [4, 55, 22]
+        # array([[357, 397, 356, 141, 395]])
+    indexes = find_nearest_neighbor_indexes(x, features)
+    # Create list
+    sum_prices = []
+    # Loop through the array
+    for i in indexes:
+        # Append the prices to the list using the index position i
+        sum_prices.append(prices[i])
+    # Average prices
+    neighbor_avg = np.mean(sum_prices)
+    # Return average
+    return neighbor_avg
+#----------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
+# Load the Boston housing dataset
+
+# #Question: Can one use one function to sperate all data into its features and outputs?
+dataset = 'housing.csv'
+data = pd.read_csv(dataset)
+prices = data['MEDV']
+features = data.drop('MEDV', axis = 1)
+# cm = np.corrcoef(data.values.T)
+# sns.set(font_scale=1.5)
+# hm = sns.heatmap(cm,
+#                 cbar=True,
+#                 annot=True,
+#                 square=True,
+#                 yticklabels=cols,
+#                 xticklabels=cols)
+
+# Shuffle and split the data into training and testing subsets
+X_train, X_test, y_train, y_test = train_test_split(features, prices, test_size=0.2, random_state = 42)
+
+# ModelLearning(features, prices)
+# ModelComplexity(features, prices)
+
+# Fit the training data to the model using grid search
+reg = fit_model(X_train, y_train)
+reg_2 = fit_model_2(X_train, y_train) 
+
+client_data = [[2, 80, 50], # Client 1
+               [4, 40, 20], # Client 2
+               [6, 10, 10]]  # Client 3
+
+index = 0
+for i in (client_data):
+    arr = np.array(i).reshape(1,-1)
+    value = nearest_neighbor_price(arr)
+    price = (reg.predict(arr))
+    price = price[0]
+    index += 1
+    print (f"\n Predicted selling price for Client {index}'s home:${round(price,0)}, compared to its neighbours at ${value}")
+    
+    
+#Sensitivity test
+PredictTrials(features, prices, fit_model, client_data)
